847d413d99cacf460adde550ea817c45caa7b6e9
albinchristo04 <priyanka0101111@gmail.com>
2025-12-13 19:02:21 +0530

    Update generate-seo.cjs

--- generate-seo.cjs
+++ generate-seo.cjs
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
+import https from "https";
 import { fileURLToPath } from "url";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
